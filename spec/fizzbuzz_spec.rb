daec3dff8cf7e1734bab50c4075cf0c6e2a68e41
Ed Courage <edcourage@live.com>
2019-10-09 14:40:05 +0100

    corrected test program

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,16 +2,13 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-
-
-
   it 'return "fizzbuzz" when passed multiples of 3 and 5' do
   arg_int = 1
     100.times do
       if arg_int % 3 == 0 && arg_int % 5 == 0
          expect(fizzbuzz(arg_int)).to eq "fizzbuzz"
-       arg_int += 1
       end
+      arg_int += 1
     end
   end
 
@@ -20,8 +17,8 @@ describe 'fizzbuzz' do
     100.times do
       if arg_int % 3 == 0 && arg_int % 5 != 0
          expect(fizzbuzz(arg_int)).to eq "fizz"
-       arg_int += 1
       end
+      arg_int += 1
     end
   end
 
@@ -30,8 +27,8 @@ describe 'fizzbuzz' do
     100.times do
       if arg_int % 3 != 0 && arg_int % 5 == 0
          expect(fizzbuzz(arg_int)).to eq "buzz"
-       arg_int += 1
-      end
+       end
+      arg_int += 1
     end
   end
 
@@ -40,26 +37,8 @@ describe 'fizzbuzz' do
       100.times do
         if arg_int % 3 != 0 && arg_int % 5 != 0
            expect(fizzbuzz(arg_int)).to eq arg_int
-         arg_int += 1
         end
+        arg_int += 1
       end
      end
-
-
 end
-
-=begin
-it 'return "buzz" when passed 3' do
-arg_int = 1
-  1000000.times do
-     if arg_int % 3 == 0 && arg_int % 5 == 0
-       expect(fizzbuzz(arg_int)).to eq "fizzbuzz"
-     elsif arg_int % 3 == 0
-       expect(fizzbuzz(arg_int)).to eq "fizz"
-     elsif arg_int % 5 == 0
-       expect(fizzbuzz(arg_int)).to eq "buzz"
-    else
-      expect(fizzbuzz(arg_int)).to eq arg_int
-     end
-     arg_int += 1
-=end
